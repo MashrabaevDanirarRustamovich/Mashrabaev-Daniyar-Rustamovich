3b4bb5fd1c32ca6a6ebb98b957a05e5d9b507349
Данияр <137296106+MashrabaevDanirarRustamovich@users.noreply.github.com>
2024-02-21 16:25:11 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN='6764074159:AAFT2QoS-0XWyRJxyP39YljD4fPCcKcMQMI'
+
+keys = {
+    "доллар":  'USD',
+    "биткоин": 'BTC',
+    "эфириум": 'ETH',
+    "евро":    'EUR',
+    "рубль":   'RUB'
+}
